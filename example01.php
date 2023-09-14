88f1d52c18f37de6dacc378d05061d179b78051b
Wanwisa Chuaiaueama <biwinner48@gmail.com>
2023-09-14 14:59:48 +0700

    1

---
+++ example01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function calSalary()
+        {
+            $month = 12*19000000;
+            echo "This is my salary".$month."dollar";
+        }
+        calSarary();
+        ?>
+</body>
+</html>
